91c78f142f9183136751f6b090ef15fd5dce3deb
dstensnes <dstensnes@>
2021-02-09 00:40:52 +0100

    Rewrite mdcconnection to use promises and queued packages

--- src/lib/protocols/mdcconnection.js
+++ src/lib/protocols/mdcconnection.js
@@ -7,59 +7,100 @@ module.exports = class MDCConnection {
         this.rhost = rhost;
         this.rport = 1515;
         this.connection = null;
-        this.recvBuffer = Uint8Array;
-        this.sendBuffer = Array();
-    }
-
-    _isConnected() {
-        return this.connection !== null;
+        this.cmdQueue = [];
+        this.recvBuffer = new Buffer.alloc(0);
+        this.processingCommand = false;
+        this.connecting = false;
+        this.reconnectTime = 1000;
+        this.cmdRate = 10;
     }
 
     _transmit() {
-        let data = this.sendBuffer.shift();
-        console.log('CONNECTION', this.connection);
-        if (data) {
-            console.log("Send> ", data);
-            this.connection.write(data, null, () => { console.log("TRANSMIT"); });
+        if (this.connecting || this.processingCommand || this.cmdQueue.length === 0) {
+            return;
+        }
+
+        if (this.connection === null) {
+            this._connect();
+            return;
+        }
+
+        this.processingCommand = true;
+        this.connection.write(this.cmdQueue[0].cmd, null);
+    }
+
+    _onReceive(data) {
+        if (this.cmdQueue.length === 0) {
+            return;
+        }
+
+        this.recvBuffer = Buffer.concat([this.recvBuffer, data]);
+        console.log("ABC> ", this.recvBuffer, this.recvBuffer[0]);
+
+        let buf = this.recvBuffer.toString();
+        let pos = buf.indexOf("\n");
+        if (pos >= 0) {
+            let cmd = this.cmdQueue.shift();
+            let result = buf.substr(0, pos);
+            this.recvBuffer = this.recvBuffer.slice(pos + 1);
+            this.processingCommand = false;
+            cmd.resolve(result);
+            setTimeout(() => { this._transmit(); }, this.cmdRate);
         }
     }
 
     _connect() {
-        this.connection = net.Socket();
-        this.connection.connect(
+        if (this.connection || this.connecting) {
+            return;
+        }
+
+        this.connecting = true;
+        let connection = net.Socket();
+
+        connection.on('error', (e) => {
+            this.connection = null;
+            this.connecting = false;
+            console.log(`${this.rhost}: `, e.code);
+            if (this.cmdQueue.length > 0) {
+                setTimeout(() => { this._connect(); }, this.reconnectTime);
+            }
+        });
+
+        connection.on('close', () => {
+            this.connection = null;
+            this.connecting = false;
+            console.log(`${this.rhost}: Disconnected`);
+            if (this.cmdQueue.length > 0) {
+                setTimeout(() => { this._connect(); }, this.reconnectTime);
+            }
+        })
+
+        connection.on('data', (data) => {
+            console.log(`${this.rhost}: DATA`, data);
+            this._onReceive(data);
+        })
+
+        connection.connect(
             this.rport,
             this.rhost,
             () => {
-                console.log(`Connected to ${this.rhost}:${this.rport}`);
-                this._transmit();
+                this.connection = connection;
+                console.log(`${this.rhost}: Connected to port ${this.rport}`);
+                this.connecting = false;
+                setTimeout(() => { this._transmit(); }, this.cmdRate);
             }
         )
-
-        this.connection.on('close', () => {
-            this.connection = null;
-        })
-
-        this.connection.on('data', (data) => {
-            console.log("Recv> ", data);
-            this.recvBuffer += data;
-        })
-
-        return this.connection;
+        return connection;
     }
 
-    _onRead(size, buf) {
-        this.recvBuffer += buf;
-        console.log("RECV> ", buf, buf[0]);
-    }
-
-    send(data) {
-        console.log("SENDBUFFER", this.sendBuffer);
-        this.sendBuffer.push(Uint8Array.from(data));
-        if (this._isConnected()) {
-            this._transmit();
-        }
-        else {
-            this._connect();
-        }
+    send(cmd) {
+        return new Promise((resolve, reject) => {
+           this.cmdQueue.push({
+               resolve: resolve,
+               reject: reject,
+               cmd: cmd,
+           });
+           this._transmit();
+        });
     }
 }
